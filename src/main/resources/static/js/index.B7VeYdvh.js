ffff6d14c800f2e2cc5c0cc15d14f0730f985a9e
quxinyu <quxinyu@efreight.cn>
2026-04-23 15:45:39 +0800

    feature: 页面替换

---
+++ src/main/resources/static/js/index.B7VeYdvh.js
@@ -0,0 +1,1 @@
+import{k as s,n as a,B as o,o as t}from"./vendor.g_PKK9dd.js";import{_ as r}from"./common.CWwNiU2x.js";import"./api.CHj5bX-3.js";import"./stores.2DK3IYrs.js";const e={class:"dashboard-container"},i=r(s({name:"首页",inheritAttrs:!1,__name:"index",setup:s=>(a(()=>{}),(s,a)=>(t(),o("div",e)))}),[["__scopeId","data-v-862fdfa8"]]);export{i as default};
